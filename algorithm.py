cb289adcb017bdfdda4b9001f169160a3dd5f2c8
jaeho Chang <glad1900@naver.com>
2022-04-05 08:42:15 +0900

    220405

--- algorithm.py
+++ algorithm.py
@@ -629,7 +629,7 @@ print(f'나눗셈을 실행한 횟수: {counter}')
 # 알고리즘개선2: 제곱근(대칭구조(5x20 과 20x5)) 이하의 어떤 소수로도 나누어 떨어지지 않는다
 counter = 0           # 곱셈과 나눗셈을 합한 횟수
 ptr = 0               # 이미 찾은 소수의 개수
-prime = [None] * 500  # 소수를 저장하는 배열
+prime = [None] * 500  # 1000개 중 짝수는 소수가 아니므로 500으로 지정
 
 prime[ptr] = 2  # 2는 소수
 ptr += 1
@@ -640,7 +640,7 @@ ptr += 1
 for n in range(5, 1001, 2):    # 홀수만을 대상으로 설정
     i = 1
     while prime[i] * prime[i] <= n:   # 제곱근 이하인지 확인
-        counter += 2
+        counter += 2           # 곱셈과 나눗셈의 산식 2개를 반영
         if n % prime[i] == 0:  # 제곱근으로 나누어 떨어지므로 소수가 아님
             break              # 반복 중단
         i += 1
@@ -651,6 +651,26 @@ for n in range(5, 1001, 2):    # 홀수만을 대상으로 설정
 
 for i in range(ptr):  # ptr개의 소수를 출력
     print(prime[i])
+print(f'곱셈과 나눗셈을 실행한 횟수: {counter}')
+
+# 알고리즘개선2-1: 배열의 원소수를 미리 결정하지 않는 방법
+
+counter = 0             # 곱셈과 나눗셈을 합한 횟수
+prime = [2, 3]          # 소수를 저장하는 배열
+
+for n in range(5, 1001, 2):     # 홀수만을 대상으로 설정
+    i = 1
+    while prime[i] * prime[i] <= n:
+        counter += 2
+        if n % prime[i] == 0:   # 나누어 떨어지므로 소수가 아님
+            break               # 반복 중단
+        i += 1
+    else:                       # 끝까지 나누어 떨어지지 않았다면
+        prime += [n]            # 소수로 배열에 등록
+        counter += 1
+
+for i in prime:                 # 소수를 출력
+    print(i)
 print(f'곱셈과 나눗셈을 실행한 횟수: {counter}')
 
 # -----------------------------------------
