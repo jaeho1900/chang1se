931e96635797ccf3b54426beb933e377a6de1ebc
jaeho Chang <glad1900@naver.com>
2022-04-22 08:40:30 +0900

    220422

--- algorithm.py
+++ algorithm.py
@@ -1427,6 +1427,108 @@ while True:
     else:
         break
 
+# # 큐
+# 데이터를 임시저장하는 선입선출 자료구조
+# 리어(맨끝)에서 인큐(넣음)작업을, 프론트(맨앞)에서 디큐(꺼냄)이 진행됨
+
+# # 링 버퍼로 큐 구현하기
+# 디큐를 하면 2번째 이후 모든 원소의 인덱스번호가 하나씩 앞으로 옮겨지며 업무복잡도가 큼
+# 이의 해결방안으로 시작인덱스번호는 프론트변수로, 마지막인덱스번호는 리얼변수의 증감으로 관리함
+
+# 고정 길이 큐 클래스 FixedQueue 구현하기
+from typing import Any
+
+class FixedQueue:
+
+    class Empty(Exception):
+        """비어 있는 FixedQueue에 대해 deque 또는 peek를 호출할 때 내보내는 예외처리"""
+        pass
+
+    class Full(Exception):
+        """가득 찬 FixedQueue에 enque를 호출할 때 내보내는 예외처리"""
+        pass
+
+    def __init__(self, capacity: int) -> None:
+        """초기화 선언"""
+        self.no = 0     # 현재 데이터 개수
+        self.front = 0  # 맨앞 원소 커서
+        self.rear = 0   # 맨끝 원소 커서
+        self.capacity = capacity      # 큐의 크기
+        self.que = [None] * capacity  # 큐의 본체
+
+    def __len__(self) -> int:
+        """큐에 있는 모든 데이터 개수를 반환"""
+        return self.no
+
+    def is_empty(self) -> bool:
+        """큐가 비어 있는지 판단"""
+        return self.no <= 0
+
+    def is_full(self) -> bool:
+        """큐가 가득 찼는지 판단"""
+        return self.no >= self.capacity
+
+    def enque(self, x: Any) -> None:
+        """데이터 x를 인큐"""
+        if self.is_full():
+            raise FixedQueue.Full  # 큐가 가득 찬 경우 예외처리를 발생
+        self.que[self.rear] = x
+        self.rear += 1             # 데이터를 넣으면서 리얼변수를 1 증가시키고
+        self.no += 1               # 전체 데이터 수도 1 증가 시킴
+        if self.rear == self.capacity:  # 리얼변수가 배열의 마지막 인덱스이면
+            self.rear = 0               # 배열의 처음(0)으로 보냄
+
+    def deque(self) -> Any:
+        """데이터를 디큐합니다"""
+        if self.is_empty():
+            raise FixedQueue.Empty  # 큐가 비어 있는 경우 예외처리를 발생
+        x = self.que[self.front]
+        self.front += 1             # 데이터를 꺼내면서 프론트변수를 1 증가시키고
+        self.no -= 1                # 전체 데이터 수는 1 감소 시킴
+        if self.front == self.capacity:  # 프론트변수가 배열의 마지막 인덱스이면
+            self.front = 0               # 배열의 처음(0)으로 보냄
+        return x
+
+# Do it! 실습 4-3 [D]
+    def peek(self) -> Any:
+        """데이터를 피크합니다(맨 앞 데이터를 들여다 봄)"""
+        if self.is_empty():
+            raise FixedQueue.Empty  # 큐가 비어 있으면 예외처리를 발생
+        return self.que[self.front]
+
+    def find(self, value: Any) -> Any:
+        """큐에서 value를 찾아 인덱스를 반환하고 없으면 -1을 반환합니다"""
+        for i in range(self.no):
+            idx = (i + self.front) % self.capacity
+            if self.que[idx] == value:  # 검색 성공
+                return idx
+        return -1  # 검색 실패
+
+    def count(self, value: Any) -> bool:
+        """큐에 포함되어 있는 value의 개수를 반환합니다"""
+        c = 0
+        for i in range(self.no):  # 큐 데이터를 선형 검색
+            idx = (i + self.front) % self.capacity
+            if self.que[idx] == value:  # 검색 성공
+                c += 1  # 들어있음
+        return c
+
+    def __contains__(self, value: Any) -> bool:
+        """큐에 value가 포함되어 있는지 판단합니다"""
+        return self.count(value)
+
+    def clear(self) -> None:
+        """큐의 모든 데이터를 비웁니다"""
+        self.no = self.front = self.rear = 0
+
+    def dump(self) -> None:
+        """모든 데이터를 맨 앞에서 맨 끝 순서로 출력합니다"""
+        if self.is_empty():  # 큐가 비어 있으면 예외처리를 발생
+            print('큐가 비어 있습니다.')
+        else:
+            for i in range(self.no):
+                print(self.que[(i + self.front) % self.capacity], end=' ')
+            print()
 
 # -----------------------------------------
 # 5장. 재귀 알고리즘
---
+++ bluescan.py
@@ -0,0 +1,145 @@
+"""
+블루스캔
+220421
+"""
+
+
+# # 스크롤 ----------
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from bs4 import BeautifulSoup as bs
+import time
+import pandas as pd
+import urllib
+
+keyword_input = '블루에셋'
+keyword_except = '스카이블루에셋'
+start_date = '2021.01.01'
+end_date = '2021.12.31'
+
+sd = start_date.replace('.','')
+ed = end_date.replace('.','')
+keyword = urllib.parse.quote(keyword_input)
+keyword2 = urllib.parse.quote(keyword_except)
+
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+driver = webdriver.Chrome(executable_path=r'D:/2022/_Py/_common/chromedriver.exe', chrome_options=options)
+url = rf"https://search.naver.com/search.naver?sm=tab_hty.top&where=news&query=%22{keyword}%22+-%22{keyword2}%22&oquery=%22{keyword}%22&tqi=hDQsCdp0Jywssl88giGsssssshN-194891&nso=so%3Ar%2Cp%3Afrom{sd}to{ed}%2Ca%3Aall&de={end_date}&ds={start_date}&mynews=0&office_section_code=0&office_type=0&pd=3&photo=0&sort=0"
+driver.get(url)
+time.sleep(3)
+html = driver.page_source
+soup = bs(html, 'html.parser')
+agency = []
+date = []
+title = []
+link = []
+for i in driver.find_elements_by_css_selector('.info.press'):
+    agency.append(i.text)
+for i in driver.find_elements_by_css_selector('span.info'):
+    if len(i.text) == 11: date.append(i.text)
+for i in driver.find_elements_by_css_selector('div.news_wrap.api_ani_send > div > a'):
+    title.append(i.text)
+for i in driver.find_elements_by_css_selector('div.news_wrap.api_ani_send > div > a'):
+    link.append(i.get_attribute('href'))
+
+# 페이지 반복 ----------
+
+while driver.find_element_by_css_selector('a.btn_next').get_attribute('aria-disabled') == 'false':
+    # 클릭 후 스크랩핑
+    driver.find_element_by_css_selector('a.btn_next').click()
+    time.sleep(3)
+    html = driver.page_source
+    soup = bs(html, 'html.parser')
+    for i in driver.find_elements_by_css_selector('.info.press'):
+        agency.append(i.text)
+    for i in driver.find_elements_by_css_selector('span.info'):
+        if len(i.text) == 11: date.append(i.text)
+    for i in driver.find_elements_by_css_selector('div.news_wrap.api_ani_send > div > a'):
+        title.append(i.text)
+    for i in driver.find_elements_by_css_selector('div.news_wrap.api_ani_send > div > a'):
+        link.append(i.get_attribute('href'))
+
+# 최종 작업 ----------
+
+df = pd.DataFrame({'언론사명': agency, '기사날짜': date, '제목': title, 'URL': link})
+df.sort_values(by='기사날짜', ascending=True, inplace=True, ignore_index=True)
+df.index = df.index + 1
+with pd.ExcelWriter("C:/Users/Administrator/Desktop/{}.xlsx".format(keyword_input)) as writer:
+    df.to_excel(writer)
+time.sleep(1)
+driver.close()
+
+
+# # 판다스 ----------
+
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+
+from matplotlib import rc
+rc('font', family = 'malgun gothic')
+
+trend_df = pd.read_excel(
+            'c:/Users/Administrator/Desktop/에스원_동향.xlsx',
+            skiprows=6,
+            parse_dates = ['날짜'])
+
+asset_df = pd.read_excel(
+            'c:/Users/Administrator/Desktop/에스원_동향.xlsx',
+            sheet_name = '블루에셋',
+            usecols='B:E',
+            parse_dates = ['기사날짜'])
+
+scan_df = pd.read_excel(
+            'c:/Users/Administrator/Desktop/에스원_동향.xlsx',
+            sheet_name = '블루스캔',
+            usecols='B:E',
+            parse_dates = ['기사날짜'])
+
+asset_count = asset_df['기사날짜'].value_counts(dropna = False)
+scan_count = scan_df['기사날짜'].value_counts(dropna = False)
+new_df = pd.merge(trend_df, asset_count, how='outer', left_on='날짜', right_index=True)
+new_df = pd.merge(new_df, scan_count, how='outer', left_on='날짜', right_index=True)
+new_df.columns = ['날짜', '블루스캔', '블루에셋', '에셋기사건수', '스캔기사건수']
+new_df.fillna(0, inplace=True)
+
+new_df.to_excel('c:/Users/Administrator/Desktop/에스원_동향.xlsx',
+            sheet_name='취합')
+
+# # 시각화 ------------------
+fig = plt.figure(figsize=(15, 10))
+ax1 = fig.add_subplot(221)
+ax2 = fig.add_subplot(222)
+ax3 = fig.add_subplot(223)
+ax4 = fig.add_subplot(224)
+
+ax1.plot(new_df['날짜'], new_df['블루에셋'], color='blue')
+ax1.set_ylabel('네이버 트랜드 추이(%)')
+ax1.set_xlim(new_df['날짜'].min()-pd.Timedelta(days=5), new_df['날짜'].max()+pd.Timedelta(days=5))
+ax1.set_ylim(0, 100)
+ax1.grid(True)
+ax1.set_title('블루에셋 검색추이')
+
+ax2.plot(new_df['날짜'], new_df['블루스캔'], color='magenta')
+ax2.set_xlim(new_df['날짜'].min()-pd.Timedelta(days=5), new_df['날짜'].max()+pd.Timedelta(days=5))
+ax2.set_ylim(0, 100)
+ax2.grid(True)
+ax2.set_title('블루스캔 검색추이')
+
+ax3.bar(new_df['날짜'], new_df['에셋기사건수'], color='blue')
+ax3.set_ylabel('기사건수(개)')
+ax3.set_xlim(new_df['날짜'].min()-pd.Timedelta(days=5), new_df['날짜'].max()+pd.Timedelta(days=5))
+ax3.set_ylim(0, 5)
+ax3.grid(True)
+ax3.set_title('블루에셋 기사건수')
+
+ax4.bar(new_df['날짜'], new_df['스캔기사건수'], color='magenta')
+ax4.set_xlim(new_df['날짜'].min()-pd.Timedelta(days=5), new_df['날짜'].max()+pd.Timedelta(days=5))
+ax4.set_ylim(0, 5)
+ax4.grid(True)
+ax4.set_title('블루스캔 기사건수')
+
+plt.suptitle("'검색 트랜드'와 '기사 건수'간 상관성", fontsize=20)
+plt.show()
